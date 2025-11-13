9541a85e2c8765ef564154c247e1b760cc343388
Jon Erickson <jon@deschutesdesigngroup.com>
2025-11-12 17:27:27 -0800

    Wip

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MaintenanceController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['domain' => config('app.url')], function (): void {
     Route::get('/', HomeController::class)->name('home');
+    Route::get('/maintenance', MaintenanceController::class)->name('maintenance');
 
     Route::get('users/{user:reference_id}', [UserController::class, 'show'])->name('users.show');
 
