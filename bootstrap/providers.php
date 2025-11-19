4cc4299c54a80d5ed901531345ab970e0f68b33f
Jon Erickson <jon@deschutesdesigngroup.com>
2025-11-19 09:49:16 -0800

    Wip

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,14 +2,22 @@
 
 declare(strict_types=1);
 
-return [
+$providers = [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\MarketplacePanelProvider::class,
-    App\Providers\HorizonServiceProvider::class,
     App\Providers\MacroServiceProvider::class,
     App\Providers\MigrationServiceProvider::class,
     App\Providers\PaymentServiceProvider::class,
     App\Providers\SupportTicketServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
 ];
+
+if (class_exists('\\Laravel\\Horizon\\Horizon')) {
+    $providers[] = App\Providers\HorizonServiceProvider::class;
+}
+
+if (class_exists('\\Laravel\\Telescope\\Telescope')) {
+    $providers[] = App\Providers\TelescopeServiceProvider::class;
+}
+
+return $providers;
