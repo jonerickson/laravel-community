78f3e4d090fca1697076833b87195486d832e155
Jon Erickson <jon@deschutesdesigngroup.com>
2025-11-29 15:46:46 -0800

    Wip

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -16,7 +16,7 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
             <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
                 <div className="absolute inset-0 bg-zinc-900" />
                 <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
-                    <AppLogoIcon className="mr-2 size-8 fill-current text-white" />
+                    <AppLogoIcon className="mr-2 size-12 fill-current text-white" />
                     {name}
                 </Link>
             </div>
@@ -25,7 +25,7 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
                 <div className="w-full p-8 lg:mx-auto lg:max-w-3xl lg:px-12">
                     <div className="flex w-full flex-col justify-center space-y-6">
                         <Link href={route('home')} className="relative z-20 flex items-center justify-center lg:hidden">
-                            <AppLogoIcon className="h-10 fill-current sm:h-12" />
+                            <AppLogoIcon className="h-18 fill-current sm:h-12" />
                         </Link>
                         <div className="flex flex-col gap-2 text-center">
                             <h1 className="text-xl font-medium">{title}</h1>
