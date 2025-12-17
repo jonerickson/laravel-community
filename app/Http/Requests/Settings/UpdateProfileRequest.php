b3f6c8f5e06cba76524bf0a73bd53bd3b610d428
Jon Erickson <jon@deschutesdesigngroup.com>
2025-12-16 17:05:54 -0800

    Prevent names from having profanity

--- app/Http/Requests/Settings/UpdateProfileRequest.php
+++ app/Http/Requests/Settings/UpdateProfileRequest.php
@@ -24,7 +24,7 @@ class UpdateProfileRequest extends FormRequest
         $fields = Field::query()->get()->mapWithKeys(fn (Field $field): array => ['fields.'.$field->id => $field->type->getRules($field)])->toArray();
 
         return array_merge($fields, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', new NoProfanity, new BlacklistRule],
             'signature' => ['nullable', 'string', 'max:500', new NoProfanity, new BlacklistRule],
             'avatar' => ['nullable', 'image', 'max:2048'],
         ]);
