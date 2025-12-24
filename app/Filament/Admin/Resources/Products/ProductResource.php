3754cd3444db930ef72ce832b7c576cadc1a4e6b
Jon Erickson <jon@deschutesdesigngroup.com>
2025-12-23 21:07:02 -0800

    Prevent both discount and promotion codes from being enabled on products

--- app/Filament/Admin/Resources/Products/ProductResource.php
+++ app/Filament/Admin/Resources/Products/ProductResource.php
@@ -310,10 +310,14 @@ class ProductResource extends Resource
                         Section::make('Purchasing')
                             ->components([
                                 Toggle::make('allow_promotion_codes')
+                                    ->afterStateUpdated(fn (Set $set) => $set('allow_discount_codes', false))
+                                    ->live()
                                     ->label('Allow Promotion Codes')
                                     ->helperText('Allow customers to use promotion codes when purchasing this product that were generated from your payment processor. You may only use promotion codes or discount codes. You may not use both.')
                                     ->columnSpanFull(),
                                 Toggle::make('allow_discount_codes')
+                                    ->afterStateUpdated(fn (Set $set) => $set('allow_promotion_codes', false))
+                                    ->live()
                                     ->label('Allow Discount Codes')
                                     ->default(true)
                                     ->helperText('Allow customers to use discount codes when purchasing this product that were generated from this platform.')
