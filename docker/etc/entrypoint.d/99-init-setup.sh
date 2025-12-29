47650f6dd0cd221a7c6a530e9157643a7c4b59b7
Jon Erickson <jon@deschutesdesigngroup.com>
2025-12-29 13:50:06 -0800

    Allow passport keys command to fail

--- docker/etc/entrypoint.d/99-init-setup.sh
+++ docker/etc/entrypoint.d/99-init-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -f "$APP_BASE_DIR/artisan" ]; then
-  php "$APP_BASE_DIR/artisan" passport:keys -n
+  php "$APP_BASE_DIR/artisan" passport:keys -n || true
   php "$APP_BASE_DIR/artisan" app:install --name="Test User" --email="test@test.com" --password="password" -n
 else
   echo "❌ Artisan file not found in $APP_BASE_DIR"
